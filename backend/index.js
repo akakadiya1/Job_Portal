1c82d187c2d4289965794f15d49ef7bc0bf2409e
akakadiya1 <akakadiya1@myseneca.ca>
2024-10-21 15:17:01 -0400

    Add User Schema

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app = express();
 
 // Running server on port 3000 [localhost:3000/home]
-// app.get("/home", (req, res)=>{
-//     return res.status(200).json({
-//         message:"Developing Job Portal",
-//         success:true
-//     })
-// });
+app.get("/home", (req, res)=>{
+    return res.status(200).json({
+        message:"Developing Job Portal",
+        success:true
+    })
+});
 
 // middleware
 app.use(express.json());
@@ -23,10 +26,10 @@ const corsOptions = {
     origin:'http//localhost:5173',
     credentials:true
 }
-
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server running at port ${PORT}`);
 })
